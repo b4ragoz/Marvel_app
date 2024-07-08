6986e65975c42c47bf4fefaf7c2ec007cd5b1a29
b4ragoz <133875298+b4ragoz@users.noreply.github.com>
2024-07-09 04:14:20 +0500

    Home screen background

--- app/src/main/java/com/example/marvel_app/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/MainActivity.kt
@@ -4,13 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Card
+
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.example.marvel_app.ui.theme.Marvel_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +48,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Marvel_appTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Background()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Background() {
+    Box(Modifier.background(Color.Black))
+    {
+        Image(
+            painter = painterResource(R.drawable.background),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(vertical = 28.dp)
+                    .width(120.dp)
+            )
+            Text(
+                text = "Choose your hero",
+                style = TextStyle(
+                    fontSize = 32.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.W800
+                )
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Marvel_appTheme {
-        Greeting("Android")
+        Background()
     }
 }
