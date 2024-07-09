5486ee8639101beeb02236be082d255f05bff54b
b4ragoz <133875298+b4ragoz@users.noreply.github.com>
2024-07-09 05:41:43 +0500

    Snap behavior

--- app/src/main/java/com/example/marvel_app/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/MainActivity.kt
@@ -1,14 +1,18 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.example.marvel_app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -25,6 +29,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 
 import androidx.compose.material3.Scaffold
@@ -69,9 +74,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Carousel(heroCard: List<HeroCard>){
+
+    val lazyListState = rememberLazyListState()
+    val snapBehavior = rememberSnapFlingBehavior(lazyListState = lazyListState)
+
     LazyRow(
         horizontalArrangement = Arrangement.spacedBy(32.dp),
         contentPadding = PaddingValues(horizontal = 32.dp),
+        state = lazyListState,
+        flingBehavior = snapBehavior
     ) {
         items(heroCard){
             heroCard -> Box(
